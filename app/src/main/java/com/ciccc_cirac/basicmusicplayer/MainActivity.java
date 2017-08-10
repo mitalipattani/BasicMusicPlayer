f634dcee3be89b6691ef13be7239b647b0ee9699
Mitali Pattani <sonarmitali19@gmail.com>
2017-08-10 14:25:19 -0700

    Added seekbar and textview

--- app/src/main/java/com/ciccc_cirac/basicmusicplayer/MainActivity.java
+++ app/src/main/java/com/ciccc_cirac/basicmusicplayer/MainActivity.java
@@ -5,18 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     MediaPlayer player;
     ImageButton playerButton, stopButton, resetButton;
     boolean play_reset = true;
+    private SeekBar seekbar;
+    private TextView tx1,tx2,tx3;
+    private double startTime = 0;
+    private double finalTime = 0;
+    public static int oneTimeOnly = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tx1 = (TextView)findViewById(R.id.textView2);
+        tx2 = (TextView)findViewById(R.id.textView3);
+        tx3 = (TextView)findViewById(R.id.textView4);
+        tx3.setText("Song.mp3");
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        seekbar.setClickable(false);
+
         // Get the button from the view
         playerButton = (ImageButton) this.findViewById(R.id.play);
         playerButton.setOnClickListener(this);
@@ -97,6 +112,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 // change the image of the buttonplay button to pause
             playerButton.setImageResource(R.drawable.pause);
             player.start();
+            finalTime = player.getDuration();
+            startTime = player.getCurrentPosition();
+
+            if (oneTimeOnly == 0) {
+                seekbar.setMax((int) finalTime);
+                oneTimeOnly = 1;
+            }
+            tx2.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                    finalTime)))
+            );
+
+            tx1.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                    startTime)))
+            );
+            startTime = player.getCurrentPosition();
+            seekbar.setProgress((int)startTime);
+            startTime = player.getCurrentPosition();
+            tx1.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                    toMinutes((long) startTime)))
+            );
+            seekbar.setProgress((int)startTime);
             Toast.makeText(this, R.string.isPlaying, Toast.LENGTH_SHORT).show();
         }
     }
